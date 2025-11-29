0dd64f578fd646994ae84acfa214ffdea554f388
Semen Boldyrev <233458958+SemenBoldyrev@users.noreply.github.com>
2025-11-29 16:43:59 +0200

    скрипт через желания деду морозу

---
+++ WpfStudyingSystem/Script/DatabaseScript/Interfaces/IStudyDatabaseAssigner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfStudyingSystem.Script.DatabaseScript.Interfaces
+{
+    public interface IStudyDatabaseAssigner
+    {
+        void AssignStudent();
+        void AssignTeacher();
+        void AssignAssignment();
+        void AssignCourse();
+    }
+}
